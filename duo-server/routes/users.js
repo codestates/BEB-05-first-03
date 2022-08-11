390f540c1d363111fd3dd3e6be82358daf722435
BrilliantCB <parkaet@gmail.com>
2022-08-11 18:24:51 +0900

    request with wallet address and set image

--- duo-server/routes/users.js
+++ duo-server/routes/users.js
@@ -7,7 +7,7 @@ router.get('/', (req,res)=>{
     //완성된 코드를 머지한후 각자의 로컬호스트에서 통신테스트를 해야함
    
 })
-router.get('/login', (req,res)=>{
+router.get('/', (req,res)=>{
     //login   //localhost:3000/login
     // if(req.id === "test" && req.password === "123")
     // {
@@ -28,7 +28,48 @@ router.post('/new', (req,res)=>{
     res.send("Create User")
 })
 router.get('/:id', (req,res) => {
+    console.log("/:id'")
+    let data = {}
     //wallet address
-    res.send(`Get User With Id ${req.params.id}`)
+    //1. getNFT by owner wallet address
+    //2. send data 
+    if(req.params.id === "0x138792f224c7c113e864013381a1e63548348aa0"){
+        data = 
+            {
+                "id": 1,
+                "title": "cbpark NFT dummy",
+                "description": "duo dummy data",
+                "price": 1499,
+                "brand": "Microsoft",
+                "category": "nft",
+                "thumbnail": "https://dummyjson.com/image/i/products/8/thumbnail.jpg",
+                "images": [
+                  "https://dummyjson.com/image/i/products/8/1.jpg",
+                  "https://dummyjson.com/image/i/products/8/2.jpg",
+                  "https://dummyjson.com/image/i/products/8/thumbnail.jpg"
+                ]
+            }
+    }
+    else if (req.params.id === "0x2Bd964197d5f5b680bF34740574Ded4b2B4aEA9d"){
+        data = 
+            {
+                "id": 2,
+                "title": "yumi.kim NFT dummy",
+                "description": "duo dummy data",
+                "price": 778787,
+                "brand": "t",
+                "category": "nft",
+                "thumbnail": "https://dummyjson.com/image/i/products/8/thumbnail.jpg",
+                "images": [
+                  "https://dummyjson.com/image/i/products/8/3.jpg",
+                  "https://dummyjson.com/image/i/products/8/4.jpg",
+                  "https://dummyjson.com/image/i/products/8/thumbnail.jpg"
+                ]
+            }
+    }
+    else{
+        data = "";
+    }
+    res.send(data)
 })
 module.exports = router
