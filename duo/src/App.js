8ed8726f52ada73cf5122cc766845ec069e7e266
BrilliantCB <parkaet@gmail.com>
2022-08-11 18:30:07 +0900

    request with wallet address and set image

--- duo/src/App.js
+++ duo/src/App.js
@@ -18,7 +18,20 @@ function App() {
   }, [])
 
   function login() {
-    let result = axios.get("http://localhost:4000/users/login").then((response)=>{console.log(response)})
+    let wallet = document.querySelector('.showAccount').innerHTML
+    // `http://localhost:4000/users/login --> 
+    //http://localhost:4000/users/0x2Bd964197d5f5b680bF34740574Ded4b2B4aEA9d
+    // 통신이 잘되요. 
+    // 아 그러면 그냥 wallet변수에 주소값을 담아서 저기 넣어주면 되겠구나 
+    // http://localhost:4000/users/0x2Bd964197d5f5b680bF34740574Ded4b2B4aEA9d
+    // 두번쨰 시도했을떄는 변수로 담은거를 호출했더니 리턴이 나와요
+    let result = axios.get(`http://localhost:4000/users/${wallet}`).then((response)=>{
+      console.log(response);
+    //  console.log(response.data.images[0]);
+      let image = response.data.images[0];
+       console.log(image);
+       document.getElementById('myNFT').src = image;
+    })
   } 
   return (
     <div className="App">
@@ -26,6 +39,9 @@ function App() {
         Login 
       </button>
       <h2>Account: <span className="showAccount"></span></h2>
+      <div id="image">
+        <img id = "myNFT" src = ""></img>
+      </div>
     </div>
   )
     // <Router>
